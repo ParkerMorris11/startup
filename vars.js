bda316102ed112a496ab7f121c688c6edfd6b192
ParkerMorris11 <92393117+ParkerMorris11@users.noreply.github.com>
2023-03-10 13:40:31 -0700

    ScoreBoard Mock | HTML CSS

---
+++ vars.js
@@ -0,0 +1,23 @@
+var Game = {};
+
+Game.config = {
+  startingPlayer: "black", // Choose 'black' or 'red'.
+  takenMsg: "This position is already taken. Please make another choice.",
+  drawMsg: "This game is a draw.",
+  winMsg: "The winner is: ",
+  countToWin: 4,
+
+  // 
+  boardLength: 6,
+  boardHeight: 5,
+};
+
+// Game State
+Game.board = [[0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0]];
+
+Game.currentPlayer = Game.config.startingPlayer;
